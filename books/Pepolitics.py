80f0bbca21d98c58eeaec2efb281e56076742d73
mrsoshining <32009350+mrsoshining@users.noreply.github.com>
2017-09-23 21:11:26 +0800

    Update and rename Pepolitics to Pepolitics.py

---
+++ books/Pepolitics.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+
+def getBook():
+    return Pepolitics
+
+class Pepolitics(BaseFeedBook):
+    title                 = u'国内新闻-人民网'
+    description           = u'国内新闻-人民网。'
+    language              = 'zh-cn'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    mastheadfile          = " "
+    coverfile             = " "
+    oldest_article        = 14400    #下载多长时间之内的文章，小于等于365则单位为天，大于365则单位为秒，0为不限制,4*60*60
+    deliver_times         = [8,12,16,20]
+    
+    feeds = [
+            (u'国内新闻', 'http://www.people.com.cn/rss/politics.xml'),
+            ]
+    
+    def fetcharticle(self, url, opener, decoder):
+        #每个URL都增加一个后缀full=y，如果有分页则自动获取全部分页
+        url += '?full=y'
+        return BaseFeedBook.fetcharticle(self,url,opener,decoder)
