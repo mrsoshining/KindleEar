bf67eef1f8a0bd754ac080041ef723add871bc40
mrsoshining <32009350+mrsoshining@users.noreply.github.com>
2017-09-23 21:18:17 +0800

    Delete moretimesbase.py

--- books/moretimesbase.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-deliver_times = [8,12,16,20] #deliver_times=[6,14,22] 6:00,14:00,22:00三次推送
-oldest_article        = 0    #下载多长时间之内的文章，小于等于365则单位为天，大于365则单位为秒，0为不限制28800,oldest_article=28800 #8*60*60
